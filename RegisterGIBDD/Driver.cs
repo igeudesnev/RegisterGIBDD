6ab321a7da7b3b2f749557618a0f485d25e6271b
Игорь Суворов <igeudesnev@gmail.com>
2017-04-09 08:02:15 +0300

    Создание класса Drivers

---
+++ RegisterGIBDD/Driver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterGIBDD
+{
+    class Driver
+    {
+        private string _surname;
+
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _drivinglicensenumber;
+
+        public int Drivinglicensenumber
+        {
+            get { return _drivinglicensenumber; }
+            set { _drivinglicensenumber = value; }
+        }
+
+        public Driver(string surname, string name, int drivinglicensenumber)
+        {
+            _surname = surname;
+            _name = name;
+            _drivinglicensenumber = drivinglicensenumber;
+        }
+    }
+}
